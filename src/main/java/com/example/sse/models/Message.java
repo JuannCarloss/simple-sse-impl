fd5e6f9400cdb0ba9f0cb04e43b052bb8763d82c
juanc <juancarlosbatistazeferino@gmail.com>
2025-08-04 21:09:15 -0300

    feat: add simple body request message

---
+++ src/main/java/com/example/sse/models/Message.java
@@ -0,0 +1,31 @@
+package com.example.sse.models;
+
+public class Message {
+
+    private String uuid;
+    private String message;
+
+    public Message() {
+    }
+
+    public Message(String uuid, String message) {
+        this.uuid = uuid;
+        this.message = message;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
